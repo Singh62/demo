74a8aadaf69af6c39faabec77e92f40d0cf38889
amdoconb018 <amdoconb018@Amdonb175.iiht.tech>
2022-07-20 14:06:27 +0530

    initial commit

---
+++ demo/src/demo/InputEx.java
@@ -0,0 +1,25 @@
+package demo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class InputEx {
+	public static void main(String[] args) {
+		try {
+			FileInputStream input = new FileInputStream("target/input1.txt");
+			int i=0;
+			while((i=input.read())!=-1) {
+				System.out.print((char)i);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+}
